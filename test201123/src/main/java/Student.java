f5408e218bf1b11ecca32e6e5a521552df74e5a1
emma <577005318@qq.com>
2020-11-23 17:59:10 +0800

    Initial commit

---
+++ test201123/src/main/java/Student.java
@@ -0,0 +1,47 @@
+public class Student {
+    private String name;
+    private String age;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, String age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
